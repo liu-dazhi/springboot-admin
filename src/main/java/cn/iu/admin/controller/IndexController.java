7285f6a300fd820de0205a54ae8c6d90216964ab
liushulun <liushulun@doorock.com>
2023-11-20 21:41:54 +0800

    增加登陆校验、选择分组接口

--- src/main/java/cn/iu/admin/controller/IndexController.java
+++ src/main/java/cn/iu/admin/controller/IndexController.java
@@ -3,6 +3,8 @@ package cn.iu.admin.controller;
 import cn.iu.admin.common.Constant;
 import cn.iu.admin.entity.Content;
 import cn.iu.admin.entity.GroupName;
+import cn.iu.admin.entity.User;
+import cn.iu.admin.exception.BusinessException;
 import cn.iu.admin.mapper.UserMapper;
 import cn.iu.admin.result.Result;
 import cn.iu.admin.service.UserService;
@@ -10,11 +12,15 @@ import cn.iu.admin.service.impl.UserServiceImpl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.sql.Wrapper;
 import java.util.List;
 import java.util.UUID;
+
+import static cn.iu.admin.exception.BusinessException.throwExceptionWithNull;
 
 @RestController
 @RequestMapping("/")
@@ -28,23 +34,28 @@ public class IndexController {
 
     /**
      * 根据分组id查询选项
-     * @param groupId
+     * @param request
      * @return
      */
     @CrossOrigin
     @GetMapping(value = "/getData")
-    public Result<List<Content>> getData(@RequestParam(value = "groupId",defaultValue = "") String groupId) {
+    public Result<List<Content>> getData(HttpServletRequest request) {
+        User user = (User)request.getSession().getAttribute("user");
+        String groupId = ObjectUtils.isEmpty(user) ? "" : user.getGroupId();
         QueryWrapper<Content> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("group_id", "".equals(groupId) ? Constant.DEFAULTGROUP : groupId);
         List<Content> list = userService.list(queryWrapper);
         return new Result<>(200, "Ok", list);
     }
 
-    /**
-     * 查询分组
-     */
-
-
+    @CrossOrigin
+    @GetMapping(value = "/getDataByGroupId")
+    public Result<List<Content>> getData(String groupId) {
+        QueryWrapper<Content> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("group_id", "".equals(groupId) ? Constant.DEFAULTGROUP : groupId);
+        List<Content> list = userService.list(queryWrapper);
+        return new Result<>(200, "Ok", list);
+    }
 
     /**
      * 插入选项
@@ -66,7 +77,10 @@ public class IndexController {
      * @return
      */
     @PostMapping(value = "/insertGroupData")
-    public Result<List<Content>> insertGroupData(@RequestBody GroupName group) {
+    public Result<List<Content>> insertGroupData(@RequestBody GroupName group,HttpServletRequest request) {
+        User user = (User) request.getSession().getAttribute("user");
+        throwExceptionWithNull(user,"对不起请先登录");
+        group.setUserId(user.getId());
         userMapper.insertGroupData(group);
         return new Result<>(200, "Ok", null);
     }
